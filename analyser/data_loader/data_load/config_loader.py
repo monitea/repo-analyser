3373310f1a88683d53a503f5504f34e40f1dc1c1
monitea <monika.morawiecka@gmail.com>
2022-05-27 11:56:18 +0200

    Update config_loader.py

--- analyser/data_loader/data_load/config_loader.py
+++ analyser/data_loader/data_load/config_loader.py
@@ -2,14 +2,14 @@ import json
 
 
 def load_repos():
-    with open("input/repos.json") as json_file:
+    with open("input/repos.json", encoding="UTF-8") as json_file:
         repos = json.load(json_file)
 
     return repos
 
 
 def load_teams():
-    with open("input/teams.json") as json_file:
+    with open("input/teams.json", encoding="UTF-8") as json_file:
         teams = json.load(json_file)
 
     return teams
